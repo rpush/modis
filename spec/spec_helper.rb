203875a791edcaf09edf0748f8d4a7744424c4bc
Ben Langfeld <blangfeld@powerhrg.com>
2024-08-01 13:11:23 -0300

    Re-configure for each example

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,13 +14,12 @@ require 'modis'
 
 Redis.raise_deprecations = true if Gem.loaded_specs['redis'].version >= Gem::Version.new('4.6.0')
 
-Modis.configure do |config|
-  config.namespace = 'modis'
-end
-
 RSpec.configure do |config|
   config.after :each do
     Modis.reset!
+    Modis.configure do |config|
+      config.namespace = 'modis'
+    end
     RSpec::Mocks.space.proxy_for(Modis).reset
     Modis.connection_pools.each do |key, _|
       Modis.with_connection(key) do |connection|
